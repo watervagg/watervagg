36f309df130cdf739fe3b782e9287109800f3011
vagg <96787831+watervagg@users.noreply.github.com>
2022-02-26 23:58:11 +0200

    Update pyminiIDE.py

--- pyminiIDE.py
+++ pyminiIDE.py
@@ -1,170 +1,55 @@
-from email.mime import base
-from tkinter import *
-from tkinter.filedialog import asksaveasfilename, askopenfilename
+from distutils import command
 import subprocess
-from turtle import title
+from email.mime import base
+#This is the most important import in all of the proggram delete that and you will get 60 or more errors
+from tkinter import *
+from tkinter import font
+#This is for the open files close file e.c.t.
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from django.forms import FilePathField
+
+#!These are all the libraries you arre going to need
 
 compiler = Tk()
 compiler.title('Pymini IDE')
 file_path = ''
+i = 1
 
-
+#TODO make an exit button
+#TODO Make color codes
 
 def set_file_path(path):
     global file_path
     file_path = path
 
-
+def EXIT() :
+    root = Tk()
+    root.title('do you want to exit?')
+    Exit = Label(root, text='Do you want to exit?')
+    YesExit = Button(root, text='(y)yes', command=compiler.destroy)
+    NoExit = Button(root, text='(n)No', command=root.destroy)
+    YesExit.pack()
+    NoExit.pack()
+    Exit.pack()
+    root.mainloop()
+ 
+#This is the html tutorial its really long but
 def HtmlTutorial() :
     root = Tk()
     root.title('HTMLTutorial')
-    titleLabel = Label(root, text="""
-
-\n\n\n\n
-    HTML stands for HyperText Markup Language. It is used to design web pages using a markup language. It is the combination of Hypertext and Markup language. 
-    Hypertext defines the link between the web pages. A markup language is used to define the text document within tag which defines the structure of web pages. 
-    It is a markup language that is used by the browser to manipulate text, images, and other content to display in the required format.
-    """)
-    BaseOfHTML = Label(root, text="""
-    \n\n\n
-    Why to use HTML ?
-It helps to structure our website well. The way a skeleton system gives a structure to the human body, in a similar manner, it acts as a skeleton for a website, 
-without it a website cannot be made. If you want to work as a Software Developer especially in the Web Development domain, then learning HTML is a must, because without knowledge of it you cannot build a website.
-
-Base for creating websites: It is the basic necessity a developer should know while building a website from scratch.
-Learn web development: It is the first step towards learning Web Development. Once you learn it, you can build simple,
-static websites very easily.
-Can become freelancer: Since web development has the best scope in freelancing, therefore learning it will
-surely help you to get the best deals of website development in the market.
- 
-
-Basic Format: It is the basic format of create a simple web page.
-
-<!DOCTYPE html>
-<html>
- 
-<head>
-    <!-- Head section of website-->
-    <title></title>
-</head>
- 
-<body>
-    <!-- Body section of website -->
-</body>
- 
-</html><div class="open_grepper_editor" title="Edit & Save To Grepper"></div>
-Example: Let’s see a small example of simple web page that display the heading and paragraph content.
-
-<!DOCTYPE html>
-<html>
- 
-<head>
-    <title>Simple Web Page</title>
-</head>
- 
-<body>
-    <h1>Welcome to GeeksforGeeks</h1>
-    <p>A computer science portal for geeks</p>
-</body>
- 
-
-""", font='Helvetica 5 bold')
+    from Tutorial import HTMLtitle
+    titleLabel = Label(root, text=HTMLtitle)
+    from Tutorial import BaseOfHTML
+    BaseOfHTML = Label(root, text=BaseOfHTML, font='Helvetica 5 bold')
     titleLabel.pack()
     BaseOfHTML.pack()
 def PythonTutorial() :
+    from Tutorial import PythonTitle
     root = Tk()
-    root.title('Python tutorial')
-    BaseOfPython = Label(root, text="""
-
-\n\n\n\n
-    Python uses indentation to highlight the blocks of code. Whitespace is used for indentation in Python.
-    All statements with the same distance to the right belong to the same block of code. If a block has to be more deeply nested,
-    it is simply indented further to the right. You can understand it better by looking at the following lines of code.
-
-    # Python program showing 
-    # indentation 
-    
-    site = 'gfg'
-    
-    if site == 'gfg': 
-        print('Logging on to pymini...') 
-    else: 
-        print('retype the URL.') 
-    print('All set !') 
-    Comments are useful information that the developers provide to make the reader understand the source code.
-    It explains the logic or a part of it used in the code. There are two types of comment in Python:
-
-    Single line comments: Python single line comment starts with hashtag symbol with no white spaces.
-    # This is a comment 
-    # Print “pymini !” to console 
-    print("Pymini")
-    Multi-line string as comment: Python multi-line comment is a piece of text enclosed in a delimiter (“””) on each end of the comment.
-
-    """
-    """
-    This would be a multiline comment in Python that 
-    spans several lines and describes geeksforgeeks. 
-    A Computer Science portal for geeks. It contains  
-    well written, well thought  
-    and well-explained computer science  
-    and programming articles,  
-    quizzes and more.  
-    … 
-    print("pymini")
-
-    Variables in Python are not “statically typed”. We do not need to declare variables
-    before using them or declare their type.
-    A variable is created the moment we first assign a value to it.
-
-    #!/usr/bin/python 
-    
-    # An integer assignment 
-    age = 45                     
-    
-    # A floating point 
-    salary = 1456.8            
-    
-    # A string   
-    name = "John"             
-    
-    print(age) 
-    print(salary) 
-    print(name) 
-
-    Operators are the main building block of any programming language. Operators 
-    allow the programmer to perform different kinds of operations on operands.
-    These operators can be categorized based upon their different functionality:
-
-    Arithmetic operators: Arithmetic operators are used to perform mathematical operations like addition, subtraction,
-    multiplication and division.
-    # Examples of Arithmetic Operator 
-    a = 9
-    b = 4
-    
-    # Addition of numbers 
-    add = a + b 
-    # Subtraction of numbers  
-    sub = a - b 
-    # Multiplication of number  
-    mul = a * b 
-    # Division(float) of number  
-    div1 = a / b 
-    # Division(floor) of number  
-    div2 = a // b 
-    # Modulo of both number 
-    mod = a % b 
-    
-    # print results 
-    print(add) 
-    print(sub) 
-    print(mul) 
-    print(div1) 
-    print(div2) 
-    print(mod)
- 
-    """, font='Helvetica 15 bold')
-
-    BaseOfPython.pack()
+    root.title('python')
+    label = Text(root, text=PythonTitle, font='Helvetica 10 bold')
+    label.pack()
+    root.mainloop()
 
 def OpenGetStarted() :
     root = Tk()
@@ -196,7 +81,7 @@ def open_file():
 
 def save_as():
     if file_path == '':
-        path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+        path = asksaveasfilename(filetypes=[('Python Files', '*.py'), ("HTM5 Files", "*.html"), ("C Files", "*.c"), ("C++ Files", "*.cpp"), ("css Files", "*.css"), ("JavaScript Files", "*.js")])
     else:
         path = file_path
     with open(path, 'w') as file:
@@ -216,16 +101,26 @@ def run():
     output, error = process.communicate()
     code_output.insert('1.0', output)
     code_output.insert('1.0',  error)
+    root = Tk()
+    root.title('run')
+    label = Label(root, text="Please type this on windows powershell")
+    # label2 = Text(root, text=file_path, font='Helvetica 10 bold')
+    label.pack()
+    # label2.pack()
+    root.mainloop()
 
 
 textEditor = Text(width=400, height=200 )
-textEditor.config(bg='#362f2e', fg='#d2ded1', insertbackground='white', font=('georgia 20'))
+textEditor.config(bg='#FFFFFF', fg='#000000', insertbackground='#0000FF', font=('georgia 20'))
 textEditor.pack()
 
-
 #  This is the text size
+def twentyS(e):
+    textEditor.config(width=1000, height=500,  font=('Georgia 20'))
 def twenty():
     textEditor.config(width=1000, height=500,  font=('Georgia 20'))
+def fifteenS(e):
+    textEditor.config(width=1000, height=500, font=('Georgia 15'))
 def fifteen():
     textEditor.config(width=1000, height=500, font=('Georgia 15'))
 def fourteen():
@@ -236,6 +131,8 @@ def twelve():
     textEditor.config(width=1000, height=500, font=('Georgia 12'))
 def eleven():
     textEditor.config(width=1000, height=500, font=('Georgia 11'))
+def tenS(e):
+    textEditor.config(width=1000, height=500, font=('Georgia 10'))
 def ten():
     textEditor.config(width=1000, height=500, font=('Georgia 10'))
 def five():
@@ -252,13 +149,14 @@ def MyTextSize():
     Radiobutton(root, text='11', command=eleven, variable=v, value=6).pack(anchor=W)
     Radiobutton(root, text='10', command=ten, variable=v, value=7).pack(anchor=W)
     Radiobutton(root, text='5', command=five, variable=v, value=8).pack(anchor=W)
-    compiler.bind("Ctrl +", command=twenty)
-    compiler.mainloop
-    compiler.bind("Ctrl -", command=ten)
-    compiler.mainloop
-    compiler.bind("Ctrl =", command=fifteen)
-    compiler.mainloop
+    root.bind('<Control-p>', twentyS)
+    root.bind('<Control-m>', tenS)
     mainloop()
+#these are all the shortcuts
+compiler.bind('<Control-p>', twentyS)
+compiler.bind('<Control-m>', tenS)
+compiler.bind('<Control-l>', fifteenS)
+
 
 output = Text(height=0)
 output.config(bg='#362f2e', fg='#1dd604')
@@ -274,18 +172,31 @@ def HackerMan():
     textEditor.config(bg='black', fg='green', insertbackground='blue')
 def yellow():
     textEditor.config(bg='yellow', fg='green', insertbackground='blue')
+def darkPython():
+    textEditor.config(bg='gray', fg='#ADD8E6', insertbackground='white')
+def lightHTML():
+    textEditor.config(bg='white', fg='orange', insertbackground='#ADD8E6')
+def lightHTML():
+    textEditor.config(bg='gray', fg='orange', insertbackground='#ADD8E6')
+def cAndCpp():
+    textEditor.config(bg="gray", fg="magenta", insertbackground="black", font=('Helvetica', 15, 'bold'))
+
 def OpenMyThemes():
     #This will make an other window
     root = Tk()
     root.title("IDE settings")
     w = Label(root, text='Here are all the themes')
     v = IntVar()
-    #*  the choices
+    #the choices
     Radiobutton(root, text='light', command=light, variable=v, value=1).pack(anchor=W)
     Radiobutton(root, text='dark', command=dark, variable=v, value=2).pack(anchor=W)
     Radiobutton(root, text='magenta', command=magenta, variable=v, value=3).pack(anchor=W)
     Radiobutton(root, text='hacker theme', command=HackerMan, variable=v, value=4).pack(anchor=W)
     Radiobutton(root, text='yellow', command=yellow, variable=v, value=5).pack(anchor=W)
+    Radiobutton(root, text='Dark + Python', command=darkPython, variable=v, value=6).pack(anchor=W)
+    Radiobutton(root, text='light + html', command=lightHTML, variable=v, value=7).pack(anchor=W)
+    Radiobutton(root, text='dark + html', command=lightHTML, variable=v, value=7).pack(anchor=W)
+    Radiobutton(root, text='dark + C/C++', command=cAndCpp, variable=v, value=8).pack(anchor=W)
     mainloop()    
 
 
@@ -318,17 +229,30 @@ compiler.config(menu=menu_bar)
 editor = Text()
 editor.pack()
 
-
-
 HelpMenu = Menu(menu_bar, tearoff=0)
 HelpMenu.add_command(label='Get started', command=OpenGetStarted)
-HelpMenu.add_command(label='', command=OpenMyThemes)
+HelpMenu.add_command(label='')
 menu_bar.add_cascade(label='Help', menu=HelpMenu)
 
 compiler.config(menu=menu_bar)
 
 editor = Text()
 editor.pack()
+
+run_bar = Menu(menu_bar, tearoff=0)
+run_bar.add_command(label='Do you want to exit?', command=EXIT)
+menu_bar.add_cascade(label='Exit', menu=run_bar)
+
+compiler.config(menu=menu_bar)
+
+menu_bar.add_cascade(label="                                                                                                                                                ")
+menu_bar.add_cascade(label="Pymini IDE", font=('Helvetica', 15, 'bold'))
+
+editor = Text()
+editor.pack()
+
+
+
 
 code_output = Text(height=10)
 code_output.pack()
